ba4f634fc32c306f1d93ea694b5fd84534881c13
juampyc <jpcastelnuovo@gmail.com>
2025-09-06 22:46:13 -0300

    Modificación

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
   JS Control de Stock Castelo – Bootstrap modals nativos + SweetAlert: confirm/TOAST
 */
-const API_BASE = "https://script.google.com/macros/s/AKfycbwuCNU6Tf7E_l16zEiDDUdI0pqbGu_VYiGLkhzF66K3q0-ZSd6dX1d850TTTQVAxw0/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbxqI4TZxWb6ZQ7bfW8k5v5zEpqe57o66zoTfxNEkcyZ74McJkrwpjeSEXK8NJSxhgRo/exec";
 
 /* ---------- API ---------- */
 async function apiGet(entity, action = "getAll", extra = {}) {
